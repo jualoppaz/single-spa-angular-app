b1731a96ecbde488b177d48fa59620586fb8b5af
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-01-26 01:49:45 +0100

    Adds needed emptyRouteComponent for single-spa-angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import {ListComponent} from './list/list.component';
 import {DetailComponent} from './detail/detail.component';
+import {EmptyRouteComponent} from './empty-route/empty-route.component';
 
 const routes: Routes = [
   { path: '', component: ListComponent },
   { path: 'detail',      component: DetailComponent },
+  { path: '**', component: EmptyRouteComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import {APP_BASE_HREF} from '@angular/common';
 import { ListComponent } from './list/list.component';
 import { DetailComponent } from './detail/detail.component';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
-    DetailComponent
+    DetailComponent,
+    EmptyRouteComponent
   ],
   imports: [
     BrowserModule,
